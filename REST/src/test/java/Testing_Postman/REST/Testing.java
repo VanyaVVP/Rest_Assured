8e9d5c9784cc0e8c87acc68e2a38b327cb0fe8aa
Ivan Poduhsko <119524278+VanyaVVP@users.noreply.github.com>
2024-12-08 22:39:29 +0300

    Update Testing.java

--- REST/src/test/java/Testing_Postman/REST/Testing.java
+++ REST/src/test/java/Testing_Postman/REST/Testing.java
@@ -14,52 +14,83 @@ class Testing {
 	@Test
 	void testGET() {
 		given() 
-				.param("foo1", "bar1").param("foo2", "bar2").when().get("https://postman-echo.com/get") 
-
-				.then() 
-				.statusCode(200).body("args.foo1", equalTo("bar1")).body("args.foo2", equalTo("bar2"));
+			.param("foo1", "bar1")	
+			.param("foo2", "bar2")
+		.when()
+			.get("https://postman-echo.com/get") 
+		.then() 
+			.statusCode(200)
+			.body("args.foo1", equalTo("bar1"))
+			.body("args.foo2", equalTo("bar2"));
 	}
 
 	@DisplayName("POST - запрос RAW")
 	@Test
 	void testPOST_Raw() {
-		given().contentType(ContentType.JSON).body("This is expected to be sent back as part of response body.").when()
-				.post("https://postman-echo.com/post").then().statusCode(200)
-				.body("data", equalTo("This is expected to be sent back as part of response body."));
+		given()
+			.contentType(ContentType.JSON)
+			.body("This is expected to be sent back as part of response body.")
+		.when()
+			.post("https://postman-echo.com/post")
+		.then()
+			.statusCode(200)
+			.body("data", equalTo("This is expected to be sent back as part of response body."));
 	}
 
 	@DisplayName("POST - запрос DATA")
 	@Test
 	void testPOST_Date() {
-		Map<String, String> requestBody = new HashMap<>();
-		requestBody.put("foo1", "bar1");
-		requestBody.put("foo2", "bar2");
-		given().contentType(ContentType.JSON).body(requestBody).when().post("https://postman-echo.com/post").then()
-				.statusCode(200).body("json.foo1", equalTo("bar1")).body("json.foo2", equalTo("bar2"));
+			Map<String, String> requestBody = new HashMap<>();
+			requestBody.put("foo1", "bar1");
+			requestBody.put("foo2", "bar2");
+		given()
+			.contentType(ContentType.JSON)
+			.body(requestBody)
+		.when()
+			.post("https://postman-echo.com/post")
+		.then()		
+			.statusCode(200)
+			.body("json.foo1", equalTo("bar1"))
+			.body("json.foo2", equalTo("bar2"));
 	}
 
 	@DisplayName("PUT - запрос")
 	@Test
 	void testPUT() {
-		given().contentType(ContentType.JSON).body("This is expected to be sent back as part of response body.").when()
-				.put("https://postman-echo.com/put").then().statusCode(200)
-				.body("data", equalTo("This is expected to be sent back as part of response body."));
+		given()
+			.contentType(ContentType.JSON)
+			.body("This is expected to be sent back as part of response body.")
+		.when()
+			.put("https://postman-echo.com/put")
+		.then()
+			.statusCode(200)
+			.body("data", equalTo("This is expected to be sent back as part of response body."));
 	}
 
 	@DisplayName("PATCH - запрос")
 	@Test
 	void testPATCH() {
-		given().contentType(ContentType.JSON).body("This is expected to be sent back as part of response body.").when()
-				.patch("https://postman-echo.com/patch").then().statusCode(200)
-				.body("data", equalTo("This is expected to be sent back as part of response body."));
+		given()
+			.contentType(ContentType.JSON)
+			.body("This is expected to be sent back as part of response body.")
+		.when()
+				.patch("https://postman-echo.com/patch")
+		.then()
+			.statusCode(200)
+			.body("data", equalTo("This is expected to be sent back as part of response body."));
 	}
 
 	@DisplayName("DELETE - запрос")
 	@Test
 	void testDELETE() {
-		given().contentType(ContentType.JSON).body("This is expected to be sent back as part of response body.").when()
-				.delete("https://postman-echo.com/delete").then().statusCode(200)
-				.body("data", equalTo("This is expected to be sent back as part of response body."));
+		given()
+			.contentType(ContentType.JSON)
+			.body("This is expected to be sent back as part of response body.")
+		.when()	
+			.delete("https://postman-echo.com/delete")
+		.then()
+			.statusCode(200)		
+			.body("data", equalTo("This is expected to be sent back as part of response body."));
 	}
 
 }
